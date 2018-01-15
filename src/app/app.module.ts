d90644410d32d8cccb4d79fae8dba882e0273aa1
Alex <yixiao.chen@rutgers.edu>
2018-01-15 11:50:00 -0500

    chat only

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,36 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {AngularFireModule} from 'angularfire2';
+import { ChatPage } from '../pages/chat/chat';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+
+var config = {
+  apiKey: "AIzaSyBUJZ_Vp6BqwNBqSR41BLivIStq6TZuV3E",
+  authDomain: "chat-49f3a.firebaseapp.com",
+  databaseURL: "https://chat-49f3a.firebaseio.com",
+  projectId: "chat-49f3a",
+  storageBucket: "chat-49f3a.appspot.com",
+  messagingSenderId: "709918617697"
+};
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ChatPage,  
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ChatPage
   ],
   providers: [
     StatusBar,
